384861a2d07d12fcb34f125cf5af12ef36bbeb1c
Ahmed Jazzar <me@ahmedjazzar.com>
2017-08-25 15:11:59 +0300

    Setup travis

---
+++ fairuze/test.py
@@ -0,0 +1,11 @@
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fairuze',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+    }
+}
